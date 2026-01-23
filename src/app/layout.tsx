0b3eee7de06dac4ae5698e6a0e8e29bb136d49a9
Katangalac <katangalachristian@yahoo.com>
2026-01-23 08:26:11 -0500

    Ajout de Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/app/components/Header'
 import Footer from '@/app/components/Footer'
+import {Analytics} from "@vercel/analytics/dist/vue";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'Mon Portfolio',
+    title: 'Portfolio-Christian-Kayembe-Katangala',
     description: 'Portfolio professionnel',
 }
 
@@ -18,8 +20,11 @@ export default function RootLayout({
     return (
         <html lang="fr" className="scroll-smooth">
         <body className={inter.className}>
+        <Analytics/>
         <Header />
-        <main>{children}</main>
+        <main>
+            {children}
+        </main>
         <Footer/>
         </body>
         </html>
